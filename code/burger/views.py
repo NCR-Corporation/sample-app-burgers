64e6b64aecd1776650f0f11060935ed24384a89f
Taylor McNeil <tm185221@ncr.com>
2021-08-11 21:48:12 -0400

    Small Updates to fix conflicts

--- code/burger/views.py
+++ code/burger/views.py
@@ -9,24 +9,36 @@ from django.http import HttpResponse
 from django.conf import settings
 import datetime
 import json
+import auxMethods
 
 
 HIGHLAND = settings.LOCATIONS['Peachtree Burger Highland']
 SOUTHLAND = settings.LOCATIONS['Peachtree Burger Southland']
 MIDTOWN = settings.LOCATIONS['Peachtree Burger Midtown']
 
-MENUMAPPINGS={
-    'highlandLunch':'1626399748035',
-    'highlandDinner':'1628539739497',
-    'midtownLunch':'1628542092992',
-    'midtownDinner':'1628558391854',
-    'southlandLunch':'1628508502301',
+MENUMAPPINGS = {
+    'highlandLunch': '1626399748035',
+    'highlandDinner': '1628539739497',
+    'midtownLunch': '1628542092992',
+    'midtownDinner': '1628558391854',
+    'southlandLunch': '1628508502301',
     'southlandDinner': '1628508515030'
 }
 
 site = ''
 
+
 def index(request):
+    results = auxMethods.findResturantsInRange(
+        {'x': -84.38879, 'y': 33.777714}, 20)
+    sites = []
+
+    for i in range(0, 3):
+        sites.append(results[i])
+
+    context = {
+        'results': sites
+    }
 
     time = datetime.datetime.now()
     hour = time.hour
@@ -35,43 +47,43 @@ def index(request):
     if 15.01 < hourAndTime < 23.59:
         request.session['time'] = 'Dinner'
     else:
-        request.session['time'] = 'Lunch'  
-    return render(request, 'index.html')
+        request.session['time'] = 'Lunch'
+
+    return render(request, 'index.html', context)
 
 
 def menu(request):
-
     time = request.session.get('time')
     menustring = site+time
-    menuMapping = MENUMAPPINGS[menustring]    
-    
+    menuMapping = MENUMAPPINGS[menustring]
+
     url = f'https://gateway-staging.ncrcloud.com/menu/v2/menu-details/{menuMapping}'
 
     if site == 'highlands':
-        conn = requests.get(url,auth=(HMACAuth(HIGHLAND)))
+        conn = requests.get(url, auth=(HMACAuth(HIGHLAND)))
     elif site == 'midtown':
-        conn = requests.get(url,auth=(HMACAuth(MIDTOWN)))
+        conn = requests.get(url, auth=(HMACAuth(MIDTOWN)))
     else:
-        conn = requests.get(url,auth=(HMACAuth(SOUTHLAND)))
-    
+        conn = requests.get(url, auth=(HMACAuth(SOUTHLAND)))
+
     results = menuParsing(conn.json())
     request.session[f'{location}{time}'] = results
-    context = {'items':results,'time':time,'site':site}
+    context = {'items': results, 'time': time, 'site': site}
 
-    return render (request,'menu.html',context)
+    return render(request, 'menu.html', context)
 
-    
+
 def location(request):
     if request.method == 'POST':
-        global site 
+        global site
         body = json.loads(request.body)
         site = body['Site']
         print(f'Site Selction:{site}')
 
     return HttpResponse()
 
-def itemDetails(request, itemId,location,tag,time):   
-    
+
+def itemDetails(request, itemId, location, tag, time):
     menu = None
     context = None
 
@@ -81,7 +93,7 @@ def itemDetails(request, itemId,location,tag,time):
         elif location == 'midtown':
             menu = request.session.get('midtownLunch')
         else:
-             menu = request.session.get('southlandLunch')     
+            menu = request.session.get('southlandLunch')
     else:
         if location == 'highland':
             menu = request.session.get('highlandDinner')
@@ -89,14 +101,15 @@ def itemDetails(request, itemId,location,tag,time):
             menu = request.session.get('midtownDinner')
         else:
             menu = request.session.get('southlandDinner')
-   
+
     length = len(menu[tag])
     for item in range(length):
-       
+
         if menu[tag][item]['id'] == int(itemId):
-             context = {'item':menu[tag][item]}  
-                         
-    return render(request,'itemDetails.html',context)
+            context = {'item': menu[tag][item]}
+
+    return render(request, 'itemDetails.html', context)
+
 
 def payment(request):
     return render(request, 'payment.html')
@@ -107,11 +120,11 @@ def viewCart(request):
 
 
 def confirmation(request):
-
     userCart = request.POST.getlist('cart')
     context = {'cart': userCart}
 
     return render(request, 'confirmation.html', context)
+
 
 def about(request):
     return render(request, 'about.html')
--- code/static/js/index.js
+++ code/static/js/index.js
@@ -1,5 +1,5 @@
 let map
- 
+
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: new google.maps.LatLng(33.777714, -84.38879),
@@ -57,28 +57,44 @@ inputs.forEach(function (input) {
   });
 });
 
-$(document).ready(function(){
-    
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
 
-    $("#menu").click(function(){
-        var site = "southland"
-        $.ajax({
-            type:"POST",
-            url:'/burger/location',
-            headers:{"X-CSRFToken": "{{csrf_token}}"},
-            contentType:"application/json;charset=utf-8",
-            data: JSON.stringify({'Site':site}),
-            success:function(data){
-                console.log(data);
+$(document).ready(function () {
 
-            },
-            complete:function(){
-                window.location.href = "/burger";
-            },
-            error:function (xhr, textStatus, thrownError){
-                alert("Could not send URL to Django. Error: " + xhr.status + ": " + xhr.responseText);
-            }
-        });
+
+  $("#menu").click(function () {
+    var site = "southland"
+    var csrftoken = getCookie('csrftoken');
+    $.ajax({
+      type: "POST",
+      url: '/burger/location',
+      headers: { "X-CSRFToken": csrftoken },
+      contentType: "application/json;charset=utf-8",
+      data: JSON.stringify({ 'Site': site }),
+      success: function (data) {
+        console.log(data);
+
+      },
+      complete: function () {
+        window.location.href = "/burger";
+      },
+      error: function (xhr, textStatus, thrownError) {
+        alert("Could not send URL to Django. Error: " + xhr.status + ": " + xhr.responseText);
+      }
     });
+  });
 });
-
