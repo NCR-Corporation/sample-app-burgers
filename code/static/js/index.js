8b522624e296bfb9d4f4ae90cb2d0f64b5d11e3e
Chase Olson <co185063@WUSCO185063-0HD>
2021-08-18 14:11:28 -0400

    feature/MARKET-5125 added the functionality for the buttons in the bottom of the home page to set the location

--- code/static/js/index.js
+++ code/static/js/index.js
@@ -1,5 +1,5 @@
-let map
- 
+let map;
+
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: new google.maps.LatLng(33.777714, -84.38879),
@@ -35,6 +35,11 @@ function initMap() {
     }
 }
 
+function setLocation(data) {
+    sessionStorage.setItem("Location", data);
+    document.getElementById("store-location").innerHTML =
+        sessionStorage.getItem("Location");
+}
 
 $(document).ready(function () {
     $('input[rel="inputAddress"]').popover();
@@ -58,12 +63,14 @@ inputs.forEach(function (input) {
 
 function getCookie(name) {
     var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
+    if (document.cookie && document.cookie !== "") {
+        var cookies = document.cookie.split(";");
         for (var i = 0; i < cookies.length; i++) {
             var cookie = jQuery.trim(cookies[i]);
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            if (cookie.substring(0, name.length + 1) === name + "=") {
+                cookieValue = decodeURIComponent(
+                    cookie.substring(name.length + 1)
+                );
                 break;
             }
         }
@@ -72,32 +79,35 @@ function getCookie(name) {
 }
 
 $(document).ready(function () {
-
     $("#menu").click(function () {
-        var site = "midtown"
-        var csrftoken = getCookie('csrftoken');
+        var site = "midtown";
+        var csrftoken = getCookie("csrftoken");
         $.ajax({
             type: "POST",
-            url: '/burger/location',
+            url: "/burger/location",
             headers: { "X-CSRFToken": csrftoken },
             contentType: "application/json;charset=utf-8",
-            data: JSON.stringify({ 'Site': site }),
-            success: function (data) {
-            },
+            data: JSON.stringify({ Site: site }),
+            success: function (data) {},
             complete: function () {
                 window.location.href = "/burger/menu";
             },
             error: function (xhr, textStatus, thrownError) {
-                alert("Could not send URL to Django. Error: " + xhr.status + ": " + xhr.responseText);
-            }
+                alert(
+                    "Could not send URL to Django. Error: " +
+                        xhr.status +
+                        ": " +
+                        xhr.responseText
+                );
+            },
         });
     });
 
-   // Remove the class when the input becomes valid.
-  // 'input' will fire each time the user types
-  input.addEventListener("input", function () {
-    if (input.validity.valid) {
-      input.classList.remove(invalidClassName);
-    }
-  });
+    // Remove the class when the input becomes valid.
+    // 'input' will fire each time the user types
+    input.addEventListener("input", function () {
+        if (input.validity.valid) {
+            input.classList.remove(invalidClassName);
+        }
+    });
 });
