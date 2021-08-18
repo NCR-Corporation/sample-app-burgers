d2d6acfb79b287a13cc35b2226838db9274183f8
Chase Olson <co185063@WUSCO185063-0HD>
2021-08-18 14:38:38 -0400

    feature/MARKET-5125 eliminated unnecessary code

--- code/static/js/index.js
+++ code/static/js/index.js
@@ -41,26 +41,6 @@ function setLocation(data) {
         sessionStorage.getItem("Location");
 }
 
-$(document).ready(function () {
-    $('input[rel="inputAddress"]').popover();
-});
-
-var invalidClassName = "invalid";
-var inputs = document.querySelectorAll("input, select, textarea");
-inputs.forEach(function (input) {
-    // Add a css class on submit when the input is invalid.
-    input.addEventListener("invalid", function () {
-        input.classList.add(invalidClassName);
-    });
-    // Remove the class when the input becomes valid.
-    // 'input' will fire each time the user types
-    input.addEventListener("input", function () {
-        if (input.validity.valid) {
-            input.classList.remove(invalidClassName);
-        }
-    });
-});
-
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== "") {
@@ -101,13 +81,5 @@ $(document).ready(function () {
                 );
             },
         });
-    });
-
-    // Remove the class when the input becomes valid.
-    // 'input' will fire each time the user types
-    input.addEventListener("input", function () {
-        if (input.validity.valid) {
-            input.classList.remove(invalidClassName);
-        }
     });
 });
