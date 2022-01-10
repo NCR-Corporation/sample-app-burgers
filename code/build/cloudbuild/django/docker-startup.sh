00cfaab7e78589e033d6aed9bb0e95e2b28e4daa
Maple Buice <cb185224@ncr.com>
2022-01-10 16:13:48 -0500

    MARKET-5422 re-add SECRET_KEY

--- code/build/cloudbuild/django/docker-startup.sh
+++ code/build/cloudbuild/django/docker-startup.sh
@@ -3,6 +3,7 @@ set -e
 
 sed -i \
     -e "s/^DEBUG\s*=.*$/DEBUG = True/" \
+    -e "s/^SECRET_KEY\s*=.*$/SECRET_KEY = ['${SECRET_KEY}']/" \
     -e "s/^NEP_ORGANIZATION\s*=.*$/NEP_ORGANIZATION = '${NEP_ORGANIZATION}'/" \
     -e "s/^HMAC_SHARED_KEY\s*=.*$/HMAC_SHARED_KEY = '${HMAC_SHARED_KEY}'/" \
     -e "s/^HMAC_SECRET_KEY\s*=.*$/HMAC_SECRET_KEY = '${HMAC_SECRET_KEY}'/" \
