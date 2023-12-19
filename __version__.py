021277128080ec8be3a057737041d77dd17b6bbe
jt185289 <jt185289@ncr.com>
2023-12-19 14:50:09 -0500

    adding __version__.py file

---
+++ __version__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+__title__ = "sample-app-burgers"
+__description__ = 'sample app for demo'
+__url__ = ''
+__version__ = '0.0.1'
+__author__ = 'bsp-devex'
+__license__ = 'proprietary'
+__copyright__ = 'Copyright 2023 NCR Corporation'
