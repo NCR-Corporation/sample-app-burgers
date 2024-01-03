4166ca8822feb3bb75e3bcb39e7db7e97015a450
CORP\kk185256 <kk185256@ncr.com>
2024-01-03 13:07:38 -0600

    MARKET-7001-update-menu-page

--- code/static/js/menuPage.js
+++ code/static/js/menuPage.js
@@ -3,6 +3,7 @@ sessionStorage.setItem("Copy-Total", JSON.stringify([]));
 sessionStorage.setItem("removedItemId", JSON.stringify([]));
 
 function goToItemDetails(id, tags) {
+    console.log("GoToItemDetails")
     document.location.pathname =
         "Peachtree-Burger/Menu/ItemDetails/" + id + "/" + tags;
 }
